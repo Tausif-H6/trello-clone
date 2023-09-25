c78a089dc849127f7355ce5339ea7973df69e13a
Tausif <tsftousif2@gmail.com>
2023-09-25 20:58:07 +0800

    drop and drag working for todos.

---
+++ components/Board.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useBoardStore } from '@/store/BoardStore';
+import React, { useEffect } from 'react'
+import { DragDropContext,DropResult,Droppable } from 'react-beautiful-dnd';
+import Columns from './Columns';
+function Board() {
+  const [getBoard,board] = useBoardStore((state)=>[state.getBoard,state.board])
+  useEffect(()=>{
+    getBoard();
+  },[getBoard])
+console.log("board",board);
+const handleOnDragEnd =(result: DropResult)=>{}
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId='board' direction='horizontal' type='column'>
+        {(provided)=> <div
+        className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
+        {...provided.droppableProps}
+        ref={provided.innerRef}
+        >{
+        // Rendering columns
+        Array.from(board.colums.entries()).map(([id,column],index)=>(
+          <Columns
+            key={id}
+            id={id}
+            todos={column.todos}
+            index={index}
+          />
+        ))
+        
+        }</div> }
+        
+
+      </Droppable>
+    </DragDropContext>
+  )
+}
+
+export default Board
---
+++ typings.d.ts
@@ -0,0 +1,24 @@
+
+interface  Board{
+    colums:Map<TypedColumn,Column>
+}
+
+type TypedColumn = "todo"|"inprogress"|"done"
+
+interface Column{
+    id:TypedColumn,
+    todos:Todo[]
+}
+
+interface Todo{
+    $id:string;
+    $createdAt:string;
+    title:string;
+    status:string;
+    image?:string;
+}
+
+interface Image{
+    bucketId:string;
+    fileId:string;
+}
