043123d5e226cbb8010eb3f39518d5d0364a579f
Tausif <tsftousif2@gmail.com>
2023-09-27 13:07:44 +0800

    Delete Todos is working

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useBoardStore } from '@/store/BoardStore';
 import { XCircleIcon } from '@heroicons/react/24/solid';
 import React from 'react'
 import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from 'react-beautiful-dnd';
@@ -19,6 +20,7 @@ function TodoCard({
   draggableProps,
   dragHandleProps
 }:Props) {
+  const deleteTask = useBoardStore((state)=>state.deleteTask)
   return (
     <div
     className='bg-white rounded-md space-y-2 drop-shadow-md'
@@ -30,7 +32,7 @@ function TodoCard({
       <p>
         {todo.title}
       </p>
-      <button className='text-red-500 hover:text-red-600'>
+      <button onClick={()=> deleteTask(index,todo,id)} className='text-red-500 hover:text-red-600'>
         <XCircleIcon className='ml-5 h-8 w-8'/>
       </button>
     </div>
